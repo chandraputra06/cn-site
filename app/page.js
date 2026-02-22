953cf2e5052b53bd099586716d11c180f6e4eaed
chandraputra06 <chandra.wisesa06@gmail.com>
2026-02-22 17:47:41 +0800

    feat: add contact section with WhatsApp and Instagram CTA #13

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import ServicesOverview from "@/components/ServicesOverview";
 import PricingSection from "@/components/PricingSection";
 import PortfolioSection from "@/components/PortfolioSection";
 import FAQSection from "@/components/FAQSection";
+import ContactSection from "@/components/ContactSection";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <PricingSection />
         <PortfolioSection />
         <FAQSection />
+        <ContactSection />
       </main>
     </>
   );
